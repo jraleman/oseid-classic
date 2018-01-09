dba748f1911c4e3ba79dc3f51dc058283ecb9c63
eleriaqueen <lizard.valeth@gmail.com>
2018-01-09 18:47:57 +0100

    Batch File Update

--- compile.bat
+++ compile.bat
@@ -1,4 +1,6 @@
 gcc -O2 -s -m32 -pedantic -Wall ./source/oseidc.c -o ./release/win32/oseidc.exe
 wcl -cc -0 -bcl=dos -wx -ox ./source/oseidc.c -fe=./release/dos_8086/oseidc.exe
-upxWin32 --best ./release/win32/oseidc.exe -o./release/win32_upx/oseidc.exe
-upxwin32 --best --8086 ./release/dos_8086/oseidc.exe -o./release/dos_8086_upx/oseidc.exe
+cd ./tools/upx/
+upxWin32 --best ../../release/win32/oseidc.exe -o../../release/win32_upx/oseidc.exe
+upxwin32 --best --8086 ../../release/dos_8086/oseidc.exe -o../../release/dos_8086_upx/oseidc.exe
+cd ../..
