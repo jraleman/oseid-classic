cce3a31f3166dee9704126f5b4576820543179b3
eleriaqueen <lizard.valeth@gmail.com>
2018-06-07 18:37:37 +0200

    Minor string / macro modifications

--- source/oseidc.c
+++ source/oseidc.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 
-
 #define CHAR_ASCIITILDE 126
 #define CHAR_SPACE 32
 #define FALSE 1
 #define PSO_NAME_SIZE_MAX 12
 #define PSO_NAME_SIZE_MIN 1
+#define PSO_VALUE 5
 
-const char* TOO_FEW_ARGS = "Sorry, too few arguments. Everyone has a name, right ?";
+const char* TOO_FEW_ARGS = "Sorry, too few arguments. Your PSO character has a Name, right ?";
 const char* TOO_MANY_ARGS = "Sorry, too many arguments. \"Kireek SucksBalls\" is not a valid PSO Name !";
 const char* NAME_TOO_SHORT = "Sorry, Name is empty. Please input some seccy err... sexy Name next time !";
 const char* NAME_TOO_LONG = "Sorry, Name must contain 12 characters tops !";
-const char* NAME_NOT_ASCII = "Sorry, Name contains non-ASCII character(s), those are the worst!";
+const char* NAME_NOT_ASCII = "Sorry, Name contains non-ASCII character(s), those are the worst !";
 
 char* pso_sectionid[] = { "Pinkal", "Redria", "Oran", "Yellowboze", "Whitill", "Viridia", "Greenill", "Skyly", "Bluefull", "Purplenum" };
 
@@ -52,8 +52,7 @@ unsigned int pso_sum(char* input_str, unsigned int cval)
 
 char *sectionid(char *str)
 {
-	const int pso_value = 5;
-	return pso_sectionid[pso_sum(str, pso_value)];
+	return pso_sectionid[pso_sum(str, PSO_VALUE)];
 }
 
 int main(int argc, char **argv)
