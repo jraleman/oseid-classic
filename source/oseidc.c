f5db7f0d9d1f1f5fbae143a7cd55c90e87dd7979
eleriaqueen <lizard.valeth@gmail.com>
2018-06-07 08:38:28 +0200

    Add my_strlen(), use puts() instead of printf, remove string.h dep

--- source/oseidc.c
+++ source/oseidc.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <string.h>
+
 
 #define CHAR_ASCIITILDE 126
 #define CHAR_SPACE 32
@@ -7,24 +7,33 @@
 #define PSO_NAME_SIZE_MAX 12
 #define PSO_NAME_SIZE_MIN 1
 
-const char* TOO_FEW_ARGS = "Sorry, too few arguments. Everyone has a name, right ?\n";
-const char* TOO_MANY_ARGS = "Sorry, too many arguments. \"Kireek SucksBalls\" is not a valid PSO Name !\n";
-const char* NAME_TOO_SHORT = "Sorry, Name is empty. Please input some seccy err... sexy Name next time !\n";
-const char* NAME_TOO_LONG = "Sorry, Name must contain 12 characters tops !\n";
-const char* NAME_NOT_ASCII = "Sorry, Name contains non-ASCII character(s), those are the worst!\n";
+const char* TOO_FEW_ARGS = "Sorry, too few arguments. Everyone has a name, right ?";
+const char* TOO_MANY_ARGS = "Sorry, too many arguments. \"Kireek SucksBalls\" is not a valid PSO Name !";
+const char* NAME_TOO_SHORT = "Sorry, Name is empty. Please input some seccy err... sexy Name next time !";
+const char* NAME_TOO_LONG = "Sorry, Name must contain 12 characters tops !";
+const char* NAME_NOT_ASCII = "Sorry, Name contains non-ASCII character(s), those are the worst!";
 
 char* pso_sectionid[] = { "Pinkal", "Redria", "Oran", "Yellowboze", "Whitill", "Viridia", "Greenill", "Skyly", "Bluefull", "Purplenum" };
 
-unsigned int is_ascii(char* str)
+unsigned int my_strlen(char *str)
 {
 	char* c = str;
-	while (*c) 
+	while (*c)
 	{
-		if ((*c < CHAR_SPACE) || (*c > CHAR_ASCIITILDE))
+		c++;
+	}
+	return (c - str);
+}
+
+unsigned int is_ascii(char* str)
+{
+	while (*str) 
+	{
+		if ((*str < CHAR_SPACE) || (*str > CHAR_ASCIITILDE))
 		{
 			return 1;
 		}
-		c++;
+		str++;
 	}
 	return 0;
 }
@@ -51,28 +60,28 @@ int main(int argc, char **argv)
 {
 	if (argc < 2)
 	{
-		printf(TOO_FEW_ARGS);
+		puts(TOO_FEW_ARGS);
 		return 1;
 	}
 	if (argc > 2)
 	{
-		printf(TOO_MANY_ARGS);
+		puts(TOO_MANY_ARGS);
 		return 1;
 	}
-	if (strlen(argv[1]) < PSO_NAME_SIZE_MIN)
+	if (my_strlen(argv[1]) < PSO_NAME_SIZE_MIN)
 	{
-		printf(NAME_TOO_SHORT);
+		puts(NAME_TOO_SHORT);
 		return 1;
 	}
-	if (strlen(argv[1]) > PSO_NAME_SIZE_MAX)
+	if (my_strlen(argv[1]) > PSO_NAME_SIZE_MAX)
 	{
-		printf(NAME_TOO_LONG);
+		puts(NAME_TOO_LONG);
 		return 1;
 	}
 	if (is_ascii(argv[1]) == FALSE)
 	{
-		printf(NAME_NOT_ASCII);
+		puts(NAME_NOT_ASCII);
 		return 1;
 	}
-	printf("%s\n", sectionid(argv[1])); 
+	puts(sectionid(argv[1])); 
 }
